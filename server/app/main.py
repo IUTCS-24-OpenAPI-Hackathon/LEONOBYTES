35534d3e04aa9864290c0b11ecd1393414a29f2b
Tahmid Rahman <190041237tr@gmail.com>
2024-04-26 15:04:24 +0600

    Add CORS middleware to allow cross-origin requests in main.py

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,19 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, status, HTTPException, Query
 from Countrydetails import countries,country
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,    
+    allow_origins=origins,    
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def health_check():
